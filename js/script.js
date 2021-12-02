9856a44bccca9cce30dda9be28b37cc11933760d
chiaraxs <chiaracardinale92@gmail.com>
2021-12-02 14:40:19 +0100

    commenti struttura

---
+++ js/script.js
@@ -0,0 +1,24 @@
+// per far comparire la finestra di richiesta dati serve il prompt //
+// uso il camelcase per le variabili in modo da non avere problemi e rendere tutto più leggibile //
+
+
+// richiesta nome //
+// creo variabile const non riassegnabile con sequenza stringa alfanumerica//
+
+
+
+
+// richiesta cognome //
+// creo variabile const non riassegnabile con sequenza stringa alfanumerica//
+
+
+
+// richiesta colore preferito //
+// creo variabile const non riassegnabile con sequenza stringa alfanumerica//
+
+
+
+
+// creo variabile let che si collega al mio html in modo da generare automaticamente la password-> id 'password' //
+// grazie al div id 'password' posso far stampare (far comparire nella pagina) la mia password //
+
