7b81bb7c9b6877cf0965690cb496355d4c42df8d
chiaraxs <chiaracardinale92@gmail.com>
2021-12-02 14:44:34 +0100

    pass generator created.

--- js/script.js
+++ js/script.js
@@ -1,24 +1,25 @@
 // per far comparire la finestra di richiesta dati serve il prompt //
 // uso il camelcase per le variabili in modo da non avere problemi e rendere tutto più leggibile //
 
-
 // richiesta nome //
 // creo variabile const non riassegnabile con sequenza stringa alfanumerica//
 
-
+const userName = prompt('Inserisci il tuo nome');
 
 
 // richiesta cognome //
 // creo variabile const non riassegnabile con sequenza stringa alfanumerica//
 
-
+const userLastname = prompt('Inserisci il tuo cognome');
 
 // richiesta colore preferito //
 // creo variabile const non riassegnabile con sequenza stringa alfanumerica//
 
+const userFavouriteColour = prompt('Inserisci il tuo colore preferito');
 
 
-
-// creo variabile let che si collega al mio html in modo da generare automaticamente la password-> id 'password' //
+// creo variabile let che si collega all'html -> id 'password //
 // grazie al div id 'password' posso far stampare (far comparire nella pagina) la mia password //
 
+let htmlElement = document.getElementById('password');
+htmlElement.innerHTML = `ChiaraCardinalerosso21 ${userName} ${userLastname} ${userFavouriteColour}`;
