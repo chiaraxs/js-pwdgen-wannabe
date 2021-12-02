bc672a996f6a4ed987f301ccff5b1592e5fc1014
chiaraxs <chiaracardinale92@gmail.com>
2021-12-02 16:48:30 +0100

    bonus.

---
+++ bonus/js/script.js
@@ -0,0 +1,21 @@
+// prompt //
+let yourChoise = prompt('Gradiresti una serie tv o un film?');
+let yourGenre = prompt('Inserisci il genere di film');
+let yourTime = prompt('Inserisci la durata');
+
+
+
+// alert 1 //
+let stasera;
+stasera = 'Messaggio dalla tua coscienza: vai a dormire che è meglio!';
+alert(stasera);
+
+// alert 2 //
+let areYouSure;
+areYouSure = 'Cosa avevamo detto? Fila!';
+alert(areYouSure);
+
+// alert 3 //
+let damn;
+damn = 'Ma lo sai che domani lavori?';
+alert(damn);
